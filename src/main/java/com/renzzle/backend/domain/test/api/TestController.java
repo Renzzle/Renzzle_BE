3b9c21663114235a837ff6623a1681154be09236
Sang-Min Kang <rdrniel12345@gmail.com>
2024-08-24 13:31:06 +0900

    feat: add swagger configuration?

--- src/main/java/com/renzzle/backend/domain/test/api/TestController.java
+++ src/main/java/com/renzzle/backend/domain/test/api/TestController.java
@@ -4,12 +4,14 @@ import com.renzzle.backend.domain.test.api.response.HelloResponse;
 import com.renzzle.backend.domain.test.service.TestService;
 import com.renzzle.backend.global.common.ApiResponse;
 import com.renzzle.backend.global.util.ApiUtils;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/test")
 @RequiredArgsConstructor
+@Tag(name = "Test API", description = "default server testing api")
 public class TestController {
 
     private final TestService testService;
