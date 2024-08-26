27133f7632ea5e9eedcf5429e155c40fdbaf8213
Sang-Min Kang <rdrniel12345@gmail.com>
2024-08-26 18:47:51 +0900

    feat: add save entity by jpa api feat: add validation error handler feat: add binding result util class

--- src/main/java/com/renzzle/backend/domain/test/service/TestService.java
+++ src/main/java/com/renzzle/backend/domain/test/service/TestService.java
@@ -1,13 +1,24 @@
 package com.renzzle.backend.domain.test.service;
 
+import com.renzzle.backend.domain.test.api.request.SaveEntityRequest;
 import com.renzzle.backend.domain.test.api.response.HelloResponse;
+import com.renzzle.backend.domain.test.dao.TestRepository;
+import com.renzzle.backend.domain.test.domain.TestEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TestService {
+
+    private final TestRepository testRepository;
 
     public HelloResponse getHelloResponse(String name) {
         return new HelloResponse("Hello " + name + "!");
+    }
+
+    public TestEntity saveEntity(TestEntity entity) {
+        return testRepository.save(entity);
     }
 
 }
--- src/main/java/com/renzzle/backend/global/exception/ErrorCode.java
+++ src/main/java/com/renzzle/backend/global/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"G500","서버 내부에서 에러가 발생하였습니다");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"G500","서버 내부에서 에러가 발생하였습니다"),
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "G400", "올바르지 않은 요청입니다");
 
     private final HttpStatus status;
     private final String code;
