17c5128e35f5ace9bb989a89818dd547c386a9a3
Sang-Min Kang <rdrniel12345@gmail.com>
2025-03-31 20:55:12 +0900

    feat: add search community puzzle method by using QueryDSL

---
+++ src/main/java/com/renzzle/backend/domain/puzzle/community/dao/CommunityPuzzleQueryRepository.java
@@ -0,0 +1,12 @@
+package com.renzzle.backend.domain.puzzle.community.dao;
+
+import com.renzzle.backend.domain.puzzle.community.api.request.GetCommunityPuzzleRequest;
+import com.renzzle.backend.domain.puzzle.community.domain.CommunityPuzzle;
+
+import java.util.List;
+
+public interface CommunityPuzzleQueryRepository {
+
+    List<CommunityPuzzle> searchCommunityPuzzles(GetCommunityPuzzleRequest request, Long userId);
+
+}
