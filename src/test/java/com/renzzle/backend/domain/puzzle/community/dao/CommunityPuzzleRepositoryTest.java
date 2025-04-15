722f86c094fb7b49e615377c997f66184c905d8b
Sang-Min Kang <rdrniel12345@gmail.com>
2025-04-15 19:55:03 +0900

    test: complete community puzzle repository test /#52

--- src/test/java/com/renzzle/backend/domain/puzzle/community/dao/CommunityPuzzleRepositoryTest.java
+++ src/test/java/com/renzzle/backend/domain/puzzle/community/dao/CommunityPuzzleRepositoryTest.java
@@ -3,16 +3,17 @@ package com.renzzle.backend.domain.puzzle.community.dao;
 import com.renzzle.backend.config.DataJpaTestWithInitContainers;
 import com.renzzle.backend.domain.puzzle.community.api.request.GetCommunityPuzzleRequest;
 import com.renzzle.backend.domain.puzzle.community.domain.CommunityPuzzle;
-import com.renzzle.backend.domain.puzzle.community.domain.UserCommunityPuzzle;
-import com.renzzle.backend.domain.puzzle.shared.domain.WinColor;
 import com.renzzle.backend.domain.user.dao.UserRepository;
 import com.renzzle.backend.domain.user.domain.UserEntity;
-import com.renzzle.backend.global.common.constant.SortOption;
+import com.renzzle.backend.global.common.domain.Status;
 import com.renzzle.backend.support.TestCommunityPuzzleBuilder;
+import com.renzzle.backend.support.TestUserCommunityPuzzleBuilder;
 import com.renzzle.backend.support.TestUserEntityBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import static com.renzzle.backend.domain.puzzle.shared.domain.WinColor.getWinColor;
@@ -30,14 +31,6 @@ public class CommunityPuzzleRepositoryTest {
     @Autowired
     private UserCommunityPuzzleRepository userCommunityPuzzleRepository;
 
-    /*
-    테스트 케이스 시나리오
-    1. 동시에 모든 조건을 걸었을때 제대로 반환되는지 확인
-    2. 아이디와 사이즈를 넣었을때 그 아이디 이후로 원하는 사이즈 만큼 반환되는지 확인 (커서 방식 동작 확인)
-    3. 정렬이 제대로 되서 반환되는지 확인 (좋아요, 최신순)
-    4. 흑 문제만 조회할때 정렬이 좋아요 순일때 동일한 좋아요 중간에서 잘릴때 그 아이디를 넣어서 요청하면 제대로 나머지들이 반환되는지 확인
-     */
-
     @Test
     public void searchCommunityPuzzles_WhenVariousConditions_ThenReturnsExpectedResults() {
         // Given
@@ -49,13 +42,10 @@ public class CommunityPuzzleRepositoryTest {
                 .withDepth(7)
                 .withVerified(true)
                 .save(communityPuzzleRepository);
-        userCommunityPuzzleRepository.save(
-                UserCommunityPuzzle.builder()
-                        .user(user)
-                        .puzzle(puzzle)
-                        .isSolved(true)
-                        .build()
-        );
+
+        TestUserCommunityPuzzleBuilder.builder(user, puzzle)
+                .withSolved(true)
+                .save(userCommunityPuzzleRepository);
 
         // When
         List<CommunityPuzzle> result = communityPuzzleRepository.searchCommunityPuzzles(
@@ -123,6 +113,177 @@ public class CommunityPuzzleRepositoryTest {
         assertThat(result)
                 .extracting("id")
                 .containsExactly(puzzleC.getId(), puzzleB.getId(), puzzleA.getId());
+    }
+
+    @Test
+    public void searchCommunityPuzzles_WhenSortIsLatest_ThenOrderedByCreatedAtDesc() {
+        // Given
+        UserEntity user = TestUserEntityBuilder.builder().save(userRepository);
+
+        CommunityPuzzle puzzleA = TestCommunityPuzzleBuilder.builder(user)
+                .save(communityPuzzleRepository);
+        CommunityPuzzle puzzleB = TestCommunityPuzzleBuilder.builder(user)
+                .save(communityPuzzleRepository);
+        CommunityPuzzle puzzleC = TestCommunityPuzzleBuilder.builder(user)
+                .save(communityPuzzleRepository);
+
+        // When
+        List<CommunityPuzzle> result = communityPuzzleRepository.searchCommunityPuzzles(
+                new GetCommunityPuzzleRequest(null, 3, "LATEST", null, null, null, null, null, null),
+                user.getId()
+        );
+
+        // Then
+        assertThat(result)
+                .extracting("id")
+                .containsExactly(puzzleC.getId(), puzzleB.getId(), puzzleA.getId());
+    }
+
+    @Test
+    public void getUserLikedPuzzles_WhenCursorIsNull_ThenReturnsLikedPuzzlesSorted() {
+        // Given
+        UserEntity user = TestUserEntityBuilder.builder().save(userRepository);
+
+        CommunityPuzzle puzzle1 = TestCommunityPuzzleBuilder.builder(user).save(communityPuzzleRepository);
+        CommunityPuzzle puzzle2 = TestCommunityPuzzleBuilder.builder(user).save(communityPuzzleRepository);
+        CommunityPuzzle puzzle3 = TestCommunityPuzzleBuilder.builder(user).save(communityPuzzleRepository);
+
+        Instant now = Instant.now();
+
+        TestUserCommunityPuzzleBuilder.builder(user, puzzle1)
+                .withLiked(true)
+                .withLikedAt(now.minusSeconds(30))
+                .save(userCommunityPuzzleRepository);
+
+        TestUserCommunityPuzzleBuilder.builder(user, puzzle2)
+                .withLiked(true)
+                .withLikedAt(now.minusSeconds(20))
+                .save(userCommunityPuzzleRepository);
+
+        TestUserCommunityPuzzleBuilder.builder(user, puzzle3)
+                .withLiked(true)
+                .withLikedAt(now.minusSeconds(10))
+                .save(userCommunityPuzzleRepository);
+
+        // When
+        List<CommunityPuzzle> result = communityPuzzleRepository.getUserLikedPuzzles(user.getId(), null, 10);
+
+        // Then
+        assertThat(result)
+                .hasSize(3)
+                .extracting("id")
+                .containsExactly(puzzle3.getId(), puzzle2.getId(), puzzle1.getId()); // 최신 순
+    }
+
+    @Test
+    public void getUserLikedPuzzles_WhenCursorProvided_ThenReturnsRemainingPuzzles() {
+        // Given
+        UserEntity user = TestUserEntityBuilder.builder().save(userRepository);
+
+        CommunityPuzzle puzzle1 = TestCommunityPuzzleBuilder.builder(user).save(communityPuzzleRepository);
+        CommunityPuzzle puzzle2 = TestCommunityPuzzleBuilder.builder(user).save(communityPuzzleRepository);
+        CommunityPuzzle puzzle3 = TestCommunityPuzzleBuilder.builder(user).save(communityPuzzleRepository);
+
+        Instant now = Instant.now();
+
+        TestUserCommunityPuzzleBuilder.builder(user, puzzle1)
+                .withLiked(true)
+                .withLikedAt(now.minusSeconds(30))
+                .save(userCommunityPuzzleRepository);
+
+        TestUserCommunityPuzzleBuilder.builder(user, puzzle2)
+                .withLiked(true)
+                .withLikedAt(now.minusSeconds(20))
+                .save(userCommunityPuzzleRepository);
+
+        TestUserCommunityPuzzleBuilder.builder(user, puzzle3)
+                .withLiked(true)
+                .withLikedAt(now.minusSeconds(10))
+                .save(userCommunityPuzzleRepository);
+
+        // When
+        List<CommunityPuzzle> result = communityPuzzleRepository.getUserLikedPuzzles(user.getId(), puzzle3.getId(), 10);
+
+        // Then
+        assertThat(result)
+                .hasSize(2)
+                .extracting("id")
+                .containsExactly(puzzle2.getId(), puzzle1.getId());
+    }
+
+    @Test
+    public void getUserPuzzles_WhenCursorIsNull_ThenReturnSortedList() {
+        // Given
+        UserEntity user1 = TestUserEntityBuilder.builder().save(userRepository);
+        UserEntity user2 = TestUserEntityBuilder.builder().save(userRepository);
+
+        TestCommunityPuzzleBuilder.builder(user1)
+                .withBoardStatus("1")
+                .save(communityPuzzleRepository);
+        TestCommunityPuzzleBuilder.builder(user1)
+                .withBoardStatus("2")
+                .save(communityPuzzleRepository);
+        TestCommunityPuzzleBuilder.builder(user1)
+                .withBoardStatus("3")
+                .save(communityPuzzleRepository);
+        TestCommunityPuzzleBuilder.builder(user2)
+                .withBoardStatus("4")
+                .save(communityPuzzleRepository);
+
+        // When
+        List<CommunityPuzzle> result = communityPuzzleRepository.getUserPuzzles(user1.getId(), null, 10);
+
+        // Then
+        assertThat(result)
+                .hasSize(3)
+                .extracting("boardStatus")
+                .containsExactly("3", "2", "1");
+    }
+
+    @Test
+    public void getUserPuzzles_WhenCursorProvided_ThenReturnOnlyPreviousOnes() {
+        // Given
+        UserEntity user = TestUserEntityBuilder.builder().save(userRepository);
+
+        TestCommunityPuzzleBuilder.builder(user)
+                .withBoardStatus("A")
+                .save(communityPuzzleRepository);
+        TestCommunityPuzzleBuilder.builder(user)
+                .withBoardStatus("B")
+                .save(communityPuzzleRepository);
+        CommunityPuzzle p3 = TestCommunityPuzzleBuilder.builder(user)
+                .withBoardStatus("C")
+                .save(communityPuzzleRepository);
+
+        // When
+        List<CommunityPuzzle> result = communityPuzzleRepository.getUserPuzzles(user.getId(), p3.getId(), 10);
+
+        // Then
+        assertThat(result)
+                .hasSize(2)
+                .extracting("boardStatus")
+                .containsExactly("B", "A");
+    }
+
+    @Test
+    public void softDelete_WhenCalled_ThenPuzzleIsMarkedDeleted() {
+        // Given
+        UserEntity user = TestUserEntityBuilder.builder().save(userRepository);
+        CommunityPuzzle puzzle = TestCommunityPuzzleBuilder.builder(user)
+                .save(communityPuzzleRepository);
+
+        Instant deletedTime = Instant.now();
+
+        // When
+        int updatedCount = communityPuzzleRepository.softDelete(puzzle.getId(), deletedTime);
+
+        // Then
+        CommunityPuzzle deletedPuzzle = communityPuzzleRepository.findByIdIncludingDeleted(puzzle.getId());
+
+        assertThat(updatedCount).isEqualTo(1);
+        assertThat(deletedPuzzle.getStatus().getName()).isEqualTo(Status.StatusName.DELETED.name());
+        assertThat(deletedPuzzle.getDeletedAt().truncatedTo(ChronoUnit.MICROS))
+                .isEqualTo(deletedTime.truncatedTo(ChronoUnit.MICROS));
     }
 
 }
