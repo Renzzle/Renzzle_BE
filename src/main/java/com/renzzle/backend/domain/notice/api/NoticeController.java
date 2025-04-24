3cd6a261894801acf05df6fdc7000443cdd048c5
Sang-Min Kang <rdrniel12345@gmail.com>
2025-04-24 21:30:46 +0900

    fix: fix notice controller

--- src/main/java/com/renzzle/backend/domain/notice/api/NoticeController.java
+++ src/main/java/com/renzzle/backend/domain/notice/api/NoticeController.java
@@ -1,15 +1,17 @@
 package com.renzzle.backend.domain.notice.api;
 
-import com.renzzle.backend.domain.notice.api.request.GetNoticeWithLangCodeRequest;
+import com.renzzle.backend.domain.notice.api.request.GetPersonalNoticeRequest;
+import com.renzzle.backend.domain.notice.api.request.GetPublicNoticeRequest;
 import com.renzzle.backend.domain.notice.api.response.GetPersonalNoticeResponse;
 import com.renzzle.backend.domain.notice.api.response.GetPublicNoticeResponse;
-import com.renzzle.backend.domain.notice.api.response.GetVersionCodeResponse;
 import com.renzzle.backend.global.common.response.ApiResponse;
 import com.renzzle.backend.global.util.ApiUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/notice")
@@ -19,19 +21,13 @@ public class NoticeController {
 
     @Operation(summary = "Get personal notice", description = "Get list of personal notices")
     @PostMapping("/personal")
-    public ApiResponse<GetPersonalNoticeResponse> getPersonalNotice(@ModelAttribute GetNoticeWithLangCodeRequest request) {
+    public ApiResponse<GetPersonalNoticeResponse> getPersonalNotice(@ModelAttribute GetPersonalNoticeRequest request) {
         return ApiUtils.success(null);
     }
 
     @Operation(summary = "Get public notice", description = "Get list of public notices that not expired")
-    @PostMapping("/personal")
-    public ApiResponse<GetPublicNoticeResponse> getPublicNotice(@ModelAttribute GetNoticeWithLangCodeRequest request) {
-        return ApiUtils.success(null);
-    }
-
-    @Operation(summary = "Get app version", description = "Get list of personal notices")
-    @PostMapping("/personal")
-    public ApiResponse<GetVersionCodeResponse> getVersionCode() {
+    @PostMapping("/public")
+    public ApiResponse<List<GetPublicNoticeResponse>> getPublicNotice(@ModelAttribute GetPublicNoticeRequest request) {
         return ApiUtils.success(null);
     }
 
--- src/main/java/com/renzzle/backend/domain/notice/api/response/GetPersonalNoticeResponse.java
+++ src/main/java/com/renzzle/backend/domain/notice/api/response/GetPersonalNoticeResponse.java
@@ -2,7 +2,11 @@ package com.renzzle.backend.domain.notice.api.response;
 
 import lombok.Builder;
 
+import java.util.List;
+
 @Builder
 public record GetPersonalNoticeResponse(
-        String context
+        String description,
+        List<NoticeContext> notice,
+        String version
 ) { }
