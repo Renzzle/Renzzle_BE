958a9fe6db67fe776d589c6a87c592f8aa350365
Sang-Min Kang <rdrniel12345@gmail.com>
2025-04-24 21:30:46 +0900

    fix: fix notice controller

--- src/main/java/com/renzzle/backend/domain/notice/api/request/GetPersonalNoticeRequest.java
+++ src/main/java/com/renzzle/backend/domain/notice/api/request/GetPersonalNoticeRequest.java
@@ -3,7 +3,9 @@ package com.renzzle.backend.domain.notice.api.request;
 import com.renzzle.backend.global.common.domain.LangCode;
 import com.renzzle.backend.global.validation.ValidEnum;
 
-public record GetPublicNoticeRequest(
+public record GetPersonalNoticeRequest(
         @ValidEnum(enumClass = LangCode.LangCodeName.class, message = "잘못된 lang 형식입니다")
-        String langCode
+        String langCode,
+
+        String version
 ) { }
