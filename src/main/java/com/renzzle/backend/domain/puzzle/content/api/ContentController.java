9272d562cae5310ad15b79e118d5b0dd061ee771
tintin010 <tintin4601@gmail.com>
2025-04-24 14:43:33 +0900

    feat: add Pack Recommend API #63

---
+++ src/main/java/com/renzzle/backend/domain/puzzle/content/api/ContentController.java
@@ -0,0 +1,34 @@
+package com.renzzle.backend.domain.puzzle.content.api;
+
+import com.renzzle.backend.domain.puzzle.content.api.request.GetRecommendRequest;
+import com.renzzle.backend.domain.puzzle.content.api.response.getRecommendPackResponse;
+import com.renzzle.backend.domain.puzzle.content.service.ContentService;
+import com.renzzle.backend.global.common.response.ApiResponse;
+import com.renzzle.backend.global.security.UserDetailsImpl;
+import com.renzzle.backend.global.util.ApiUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/content")
+@RequiredArgsConstructor
+public class ContentController {
+    private final ContentService contentService;
+
+    @Operation(summary = "Get recommend Pack", description = "Get recommend Pack")
+    @GetMapping("/recommend")
+    public ApiResponse<getRecommendPackResponse> getRecommendPack(
+            @Valid @RequestBody GetRecommendRequest request,
+            @AuthenticationPrincipal UserDetailsImpl user
+    ){
+        getRecommendPackResponse response = contentService.getRecommendedPack(request, user.getUser());
+        return ApiUtils.success(response);
+    }
+
+}
