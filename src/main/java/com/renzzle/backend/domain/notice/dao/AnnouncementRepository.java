c92de699ef1a89b06bc8d754473d716a8ad44fa8
Sang-Min Kang <rdrniel12345@gmail.com>
2025-04-26 02:12:49 +0900

    feat: complete public notice API /#61

--- src/main/java/com/renzzle/backend/domain/notice/dao/AnnouncementRepository.java
+++ src/main/java/com/renzzle/backend/domain/notice/dao/AnnouncementRepository.java
@@ -1,7 +1,14 @@
 package com.renzzle.backend.domain.notice.dao;
 
 import com.renzzle.backend.domain.notice.domain.Announcement;
+import com.renzzle.backend.global.common.domain.LangCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+import java.util.List;
+
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
+
+    List<Announcement> findAllByLangCodeAndExpiredAtAfter(LangCode langCode, Instant now);
+
 }
--- src/main/java/com/renzzle/backend/domain/notice/service/NoticeService.java
+++ src/main/java/com/renzzle/backend/domain/notice/service/NoticeService.java
@@ -1,9 +1,72 @@
 package com.renzzle.backend.domain.notice.service;
 
+import com.renzzle.backend.domain.notice.api.request.GetPersonalNoticeRequest;
+import com.renzzle.backend.domain.notice.api.request.GetPublicNoticeRequest;
+import com.renzzle.backend.domain.notice.api.response.GetPersonalNoticeResponse;
+import com.renzzle.backend.domain.notice.api.response.GetPublicNoticeResponse;
+import com.renzzle.backend.domain.notice.dao.AnnouncementRepository;
+import com.renzzle.backend.domain.notice.dao.NoticeRepository;
+import com.renzzle.backend.domain.notice.dao.SystemInfoRepository;
+import com.renzzle.backend.domain.notice.domain.Announcement;
+import com.renzzle.backend.domain.notice.domain.SystemInfo;
+import com.renzzle.backend.domain.user.domain.UserEntity;
+import com.renzzle.backend.global.common.domain.LangCode;
+import com.renzzle.backend.global.exception.CustomException;
+import com.renzzle.backend.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class NoticeService {
+
+    private final Clock clock;
+    private final NoticeRepository noticeRepository;
+    private final AnnouncementRepository announcementRepository;
+    private final SystemInfoRepository systemInfoRepository;
+
+    @Transactional(readOnly = true)
+    public GetPersonalNoticeResponse getPersonalNotice(GetPersonalNoticeRequest request, UserEntity user) {
+        SystemInfo systemInfo = systemInfoRepository.getSystemInfo().orElseThrow(
+                () -> new CustomException(ErrorCode.INTERNAL_SERVER_ERROR)
+        );
+
+        if (systemInfo.isSystemCheck()) {
+            return GetPersonalNoticeResponse.builder()
+                    .description("system-check")
+                    .build();
+        }
+        if (!request.version().trim().equals(systemInfo.getVersion())) {
+            return GetPersonalNoticeResponse.builder()
+                    .description("update")
+                    .version(systemInfo.getVersion())
+                    .build();
+        }
+
+        return null;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GetPublicNoticeResponse> getPublicNotice(GetPublicNoticeRequest request) {
+        List<Announcement> activeAnnouncements = announcementRepository.findAllByLangCodeAndExpiredAtAfter(LangCode.getLangCode(request.langCode()), clock.instant());
+
+        ArrayList<GetPublicNoticeResponse> response = new ArrayList<>();
+        for (Announcement announcement : activeAnnouncements) {
+            response.add(GetPublicNoticeResponse.builder()
+                    .title(announcement.getTitle())
+                    .context(announcement.getContext())
+                    .createdAt(announcement.getCreatedAt().toString())
+                    .expiredAt(announcement.getExpiredAt().toString())
+                    .build()
+            );
+        }
+
+        return response;
+    }
+
 }
