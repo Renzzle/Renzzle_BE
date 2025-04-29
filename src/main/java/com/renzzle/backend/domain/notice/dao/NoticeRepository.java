b10ee6d172b078aa804d2cbe36c5998891053f43
Sang-Min Kang <rdrniel12345@gmail.com>
2025-04-30 00:14:58 +0900

    feat: working on personal notice API /#61

--- src/main/java/com/renzzle/backend/domain/notice/dao/NoticeRepository.java
+++ src/main/java/com/renzzle/backend/domain/notice/dao/NoticeRepository.java
@@ -1,7 +1,15 @@
 package com.renzzle.backend.domain.notice.dao;
 
 import com.renzzle.backend.domain.notice.domain.Notice;
+import com.renzzle.backend.domain.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
+    List<Notice> findAllByUser(UserEntity user);
+
+    void deleteAllByUser(UserEntity user);
+
 }
