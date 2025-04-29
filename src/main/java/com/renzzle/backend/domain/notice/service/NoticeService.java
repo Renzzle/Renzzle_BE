f57487bdc9bca2e400a506ad2509508672260fd5
Sang-Min Kang <rdrniel12345@gmail.com>
2025-04-30 00:14:58 +0900

    feat: working on personal notice API /#61

--- src/main/java/com/renzzle/backend/domain/notice/service/NoticeService.java
+++ src/main/java/com/renzzle/backend/domain/notice/service/NoticeService.java
@@ -4,10 +4,12 @@ import com.renzzle.backend.domain.notice.api.request.GetPersonalNoticeRequest;
 import com.renzzle.backend.domain.notice.api.request.GetPublicNoticeRequest;
 import com.renzzle.backend.domain.notice.api.response.GetPersonalNoticeResponse;
 import com.renzzle.backend.domain.notice.api.response.GetPublicNoticeResponse;
+import com.renzzle.backend.domain.notice.api.response.NoticeContext;
 import com.renzzle.backend.domain.notice.dao.AnnouncementRepository;
 import com.renzzle.backend.domain.notice.dao.NoticeRepository;
 import com.renzzle.backend.domain.notice.dao.SystemInfoRepository;
 import com.renzzle.backend.domain.notice.domain.Announcement;
+import com.renzzle.backend.domain.notice.domain.Notice;
 import com.renzzle.backend.domain.notice.domain.SystemInfo;
 import com.renzzle.backend.domain.user.domain.UserEntity;
 import com.renzzle.backend.global.common.domain.LangCode;
@@ -30,17 +32,19 @@ public class NoticeService {
     private final AnnouncementRepository announcementRepository;
     private final SystemInfoRepository systemInfoRepository;
 
-    @Transactional(readOnly = true)
+    @Transactional
     public GetPersonalNoticeResponse getPersonalNotice(GetPersonalNoticeRequest request, UserEntity user) {
         SystemInfo systemInfo = systemInfoRepository.getSystemInfo().orElseThrow(
                 () -> new CustomException(ErrorCode.INTERNAL_SERVER_ERROR)
         );
 
+        // System Check
         if (systemInfo.isSystemCheck()) {
             return GetPersonalNoticeResponse.builder()
                     .description("system-check")
                     .build();
         }
+        // App version check
         if (!request.version().trim().equals(systemInfo.getVersion())) {
             return GetPersonalNoticeResponse.builder()
                     .description("update")
@@ -48,7 +52,26 @@ public class NoticeService {
                     .build();
         }
 
-        return null;
+        // Get notices context
+        ArrayList<NoticeContext> contexts = new ArrayList<>();
+        // 1. Personal message
+        List<Notice> notices = noticeRepository.findAllByUser(user);
+        for (Notice notice : notices) {
+            contexts.add(NoticeContext.builder()
+                    .context(notice.getContext())
+                    .build()
+            );
+        }
+        noticeRepository.deleteAllByUser(user);
+
+        // 2. Attendance price
+
+        // 3. Badge price
+
+        return GetPersonalNoticeResponse.builder()
+                .description("context")
+                .notice(contexts)
+                .build();
     }
 
     @Transactional(readOnly = true)
---
+++ src/main/java/com/renzzle/backend/domain/notice/util/NoticeTextBuilderUtil.java
@@ -0,0 +1,9 @@
+package com.renzzle.backend.domain.notice.util;
+
+public class NoticeTextBuilderUtil {
+
+    public enum NoticeType {
+        ATTENDANCE, BADGE
+    }
+
+}
