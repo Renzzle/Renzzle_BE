73909109faa656b231a3c1271012c92422560cfc
Sang-Min Kang <rdrniel12345@gmail.com>
2025-04-30 20:44:53 +0900

    feat: complete personal notice service logic /#61

--- src/main/java/com/renzzle/backend/domain/user/dao/UserRepository.java
+++ src/main/java/com/renzzle/backend/domain/user/dao/UserRepository.java
@@ -1,5 +1,6 @@
 package com.renzzle.backend.domain.user.dao;
 
+import com.renzzle.backend.domain.user.domain.Title;
 import com.renzzle.backend.domain.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -36,5 +37,47 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     @Modifying
     @Query("UPDATE UserEntity u SET u.lastAccessedAt = :lastAccessedAt WHERE u.id = :userId")
     void updateLastAccessedAt(@Param("userId") Long userId, @Param("lastAccessedAt") Instant lastAccessedAt);
+
+    @Query("SELECT u.title FROM UserEntity u WHERE u.id = :userId")
+    Optional<Title> getUserTitle(@Param("userId") Long userId);
+
+    @Modifying
+    @Query("UPDATE UserEntity u SET u.title = :title WHERE u.id = :userId")
+    void updateUserTitle(@Param("userId") Long userId, @Param("title") Title title);
+
+    @Query(value = """
+    SELECT
+        CASE
+            WHEN 
+                (SELECT u.rating
+                 FROM user u
+                 WHERE u.id = :userId) >= :minRating
+
+            AND 
+                (SELECT COUNT(*)
+                 FROM community_puzzle cp
+                 WHERE cp.author_id = :userId AND cp.status != 'DELETED') >= :minPuzzleCount
+
+            AND 
+                (SELECT COALESCE(SUM(cp.like_count), 0)
+                 FROM community_puzzle cp
+                 WHERE cp.author_id = :userId AND cp.status != 'DELETED') >= :minLikes
+
+            AND 
+                (SELECT COALESCE(SUM(cp.solved_count), 0)
+                 FROM community_puzzle cp
+                 WHERE cp.author_id = :userId AND cp.status != 'DELETED') >= :minSolverCount
+
+            THEN TRUE
+            ELSE FALSE
+        END
+    """, nativeQuery = true)
+    boolean isUserQualified(
+            @Param("userId") Long userId,
+            @Param("minLikes") int minLikes,
+            @Param("minPuzzleCount") int minPuzzleCount,
+            @Param("minRating") double minRating,
+            @Param("minSolverCount") int minSolverCount
+    );
 
 }
