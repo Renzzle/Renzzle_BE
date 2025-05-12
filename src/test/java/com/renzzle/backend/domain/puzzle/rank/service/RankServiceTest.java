313a836c57fe1b8aadc25e886cfe9cf7d175bbcf
tintin010 <tintin4601@gmail.com>
2025-05-12 22:02:07 +0900

    test: resolve error

--- src/test/java/com/renzzle/backend/domain/puzzle/rank/service/RankServiceTest.java
+++ src/test/java/com/renzzle/backend/domain/puzzle/rank/service/RankServiceTest.java
@@ -207,7 +207,7 @@ public class RankServiceTest {
 
         CustomException ex = assertThrows(CustomException.class, () -> rankService.resultRankGame(user, request));
         // Then
-        assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.CANNOT_FIND_RANK_PUZZLE);
+        assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.LATEST_PUZZLE_NOT_FOUND);
     }
 
     @Test
