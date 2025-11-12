844766d7662feb43631d6bbab5511b52e61a0907
Sang-Min Kang <rdrniel12345@gmail.com>
2025-11-12 15:05:07 +0900

    fix: add description in GET puzzles API

--- src/main/java/com/renzzle/backend/domain/puzzle/community/api/response/GetCommunityPuzzlesResponse.java
+++ src/main/java/com/renzzle/backend/domain/puzzle/community/api/response/GetCommunityPuzzlesResponse.java
@@ -8,6 +8,7 @@ public record GetCommunityPuzzlesResponse(
         String boardStatus,
         long authorId,
         String authorName,
+        String description,
         int depth,
         String winColor,
         int solvedCount,
